c98480f1cb1d2631b531a3d6b50946bf05ab62c6
Atanas <atanasgeorgiev444@gmail.com>
2024-07-08 11:47:42 +0300

    add: implement simple home and tabs layout so we can logout

--- Frontend/app/(tabs)/home.tsx
+++ Frontend/app/(tabs)/home.tsx
@@ -1,12 +1,24 @@
-import { View, Text } from "react-native";
+import { View, Text, Alert } from "react-native";
 import React from "react";
+import CustomButton from "components/CustomButton";
+import useLogout from "../../hooks/useLogout";
+import { useRouter } from "expo-router";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useAuth } from "context/AuthContext";
+import LogoutButton from "../../components/LogoutButton";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
 
-const home = () => {
+const Home = () => {
   return (
-    <View>
-      <Text>home</Text>
-    </View>
+    <SafeAreaView className="bg-accent h-full">
+      <StatusBar style="auto" />
+      <View className="bg-secondary h-screen justify-center items-center">
+        <Text>Hello</Text>
+        <LogoutButton />
+      </View>
+    </SafeAreaView>
   );
 };
 
-export default home;
+export default Home;
